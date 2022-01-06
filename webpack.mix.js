7de7ff93f861f741a72de89ac40ffe18135ef02b
Aroushi Sharma <sharma.aroushi@gmail.com>
2022-01-06 18:39:49 +0530

    plan txt, server js , webpack changes

---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+// webpack.mix.js
+
+let mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'public/js/app.js').sass('resources/scss/app.scss', 'public/css/app.css');
+
+
+mix.babelConfig({
+    "plugins": ["@babel/plugin-proposal-class-properties"]
+});
